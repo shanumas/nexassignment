b3b63a75b24d3d230dc94f44542e4f1bcc1a1976
Uma Shankar Subramani <uma.shankar@solme.se>
2020-11-22 14:52:20 +0100

    Works fine to get the DPI records

--- nxdiag/src/main/java/com/uma/nxdiag/controller/RecordsController.java
+++ nxdiag/src/main/java/com/uma/nxdiag/controller/RecordsController.java
@@ -1,38 +1,72 @@
 package com.uma.nxdiag.controller;
 
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.uma.nxdiag.services.DPIService;
-import com.uma.nxdiag.utils.RecordUtil;
 import com.uma.nxdiag.model.DPIRecord;
-import com.uma.nxdiag.model.Record;
-
+import com.uma.nxdiag.services.DPIService;
 
 @RestController
 public class RecordsController {
-	
+
 	@Autowired
 	private DPIService dpiService;
-	
+
 	@GetMapping("/records/all")
-	public List<DPIRecord> getAll(){
+	public List<DPIRecord> getAll() {
 		return dpiService.getrecords();
 	}
-	
+
 //	@GetMapping("/clients")
 //	public Set<Integer> getClients(){
 //		return RecordUtil.getClientsList(DPIService.getAll());
 //	}
-	
-	@GetMapping("/records/client")
-	public Set<Integer> getClientRecords(@RequestParam(name = "id") int client_id){
-		return RecordUtil.getClientRecords(client_id);
+
+	// Sample http://localhost:8080/records/all
+	@GetMapping("/records/client/{id}")
+	public List<DPIRecord> getClientRecords(@PathVariable int id) {
+		return dpiService.getrecords().stream().filter(record -> record.getClient_id() == id)
+				.collect(Collectors.toList());
+	}
+
+	// Sample http://localhost:8080/records/office/88
+	@GetMapping("/records/office/{id}")
+	public List<DPIRecord> getOfficeRecords(@PathVariable int id) {
+		return dpiService.getrecords().stream().filter(record -> record.getOffice_id() == id)
+				.collect(Collectors.toList());
+	}
+
+	// Sample http://localhost:8080/records/device/1800001
+	@GetMapping("/records/device/{id}")
+	public List<DPIRecord> getDeviceRecords(@PathVariable int id) {
+		return dpiService.getrecords().stream().filter(record -> record.getDevice_id() == id)
+				.collect(Collectors.toList());
+	}
+
+	// Sample http://localhost:8080/records/dpi/lessthan/5.9
+	@GetMapping("/records/dpi/lessthan/{value}")
+	public List<DPIRecord> getDPILessThan(@PathVariable double value) {
+		return dpiService.getrecords().stream().filter(record -> record.getDpi().doubleValue() < value)
+				.collect(Collectors.toList());
+	}
+
+	// Sample http://localhost:8080/records/dpi/greaterthan/9.5
+	@GetMapping("/records/dpi/greaterthan/{value}")
+	public List<DPIRecord> getDpiGreaterThan(@PathVariable double value) {
+		return dpiService.getrecords().stream().filter(record -> record.getDpi().doubleValue() > value)
+				.collect(Collectors.toList());
+	}
+
+	// Sample http://localhost:8080/records/dpi/greaterthan/9.5
+	@GetMapping("/records/dpi/range/{value}")
+	public List<DPIRecord> getDpiRange(@PathVariable double value) {
+		return dpiService.getrecords().stream().filter(record -> record.getDpi().doubleValue() > value)
+				.collect(Collectors.toList());
 	}
 
 }
